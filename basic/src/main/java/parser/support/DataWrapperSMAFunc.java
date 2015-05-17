d05c86a556ad1e76993b35181f5183e4b7cf6f47
samuelfrench <samfrench@gmail.com>
2015-05-17 00:16:49 -0500

    upward movement function drafted

--- basic/src/main/java/parser/support/DataWrapperSMAFunc.java
+++ basic/src/main/java/parser/support/DataWrapperSMAFunc.java
@@ -1,5 +1,7 @@
 package parser.support;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
@@ -20,12 +22,50 @@ public class DataWrapperSMAFunc {
 		if(printDebug && dataWrapper.getsMACrossover().isPresent()){
 			System.out.println("addSMARangeData: overWriting existing values");
 		}
+		
+		//we will use this as one of our simple moving average points
+		ConcurrentMap<Long, Double> highLowDiff = new ConcurrentHashMap<>();
+		dataWrapper.getRecords().values().parallelStream().forEach((r)-> {
+			if(highLowDiff.put(r.getTimestamp(),r.getHigh()-r.getLow())!=null){
+				System.err.println("err");
+			};
+		});
+		
+		ConcurrentMap<Long,Double> closeOpenDiff = new ConcurrentHashMap<>();
+		dataWrapper.getRecords().values().parallelStream().forEach((r)-> {
+			if(closeOpenDiff.put(r.getTimestamp(), r.getClose() - r.getOpen())!=null){
+				System.err.println("err");
+			}
+		});
+		
+		getUpwardMovement(dataWrapper, closeOpenDiff);
+		
+		/*
 		ConcurrentMap<Long, ConcurrentMap<Long, Double>> SMAMatrix = 
 				new ConcurrentHashMap<Long, ConcurrentMap<Long, Double>>();
-		
+		*/
 		
 		
 	}
+
+	private static ConcurrentMap<Long, Boolean> getUpwardMovement(
+			ConcurrentMap<Long, Double> closeOpenDiff) {
+		
+		ConcurrentMap<Long, Boolean> upwardMovement = new ConcurrentHashMap<>();
+		closeOpenDiff.keySet().parallelStream().forEach((k)->{
+			if(closeOpenDiff.get(k)>0){
+				upwardMovement.put(k, true);
+				return;
+			}
+			upwardMovement.put(k, false);
+		});
+		return upwardMovement;
+	}
+	
+	/*
+	 * OptionalDouble 	average()
+Returns an OptionalDouble describing the arithmetic mean of elements of this stream, or an empty optional if this stream is empty.
+	 */
 	
 	//write function to return optional
 }
