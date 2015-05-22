91068212dee1a1c09207f65ca919252e399d38f7
samuelfrench <samfrench@gmail.com>
2015-05-22 01:42:18 -0500

    need to plan design

--- basic/src/main/java/parser/support/DataWrapperTrendFunc.java
+++ basic/src/main/java/parser/support/DataWrapperTrendFunc.java
@@ -1,11 +1,14 @@
 package parser.support;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedDeque;
 import java.util.concurrent.ConcurrentMap;
 import java.util.stream.Collectors;
 
+import domain.SMAMomentumBoolMatrix;
 import domain.SMAMomentumBoolMatrix.SHIFT;
 public class DataWrapperTrendFunc {
 	
@@ -15,8 +18,7 @@ public class DataWrapperTrendFunc {
 		if(sma==null){
 			throw new IllegalArgumentException("SMA Matrix is null, cannot get momentum");
 		}
-		final List<Long> tsList
-					= sma.keySet().stream().sorted().collect(Collectors.toList());
+		final List<Long> tsList = getListOfTimestamps(sma);
 		
 		
 		ConcurrentMap<Long, ConcurrentMap<Long, SHIFT>> shift = new ConcurrentHashMap<>();
@@ -49,6 +51,33 @@ public class DataWrapperTrendFunc {
 		//SMAMomentumBoolMatrix calcVals = new SMAMomentumBoolMatrix();
 		
 		return shift;
+	}
+
+
+	public static List<Long> getListOfTimestamps(
+			final ConcurrentMap<Long, ConcurrentMap<Long, Optional<Double>>> sma) {
+		final List<Long> tsList
+					= sma.keySet().stream().sorted().collect(Collectors.toList());
+		return tsList;
+	}
+	
+	/*
+	public static SMAMomentumBoolMatrix getShiftDirections(ConcurrentMap<Long, ConcurrentMap<Long, SHIFT>> baseData, 
+			final ConcurrentMap<Long, ConcurrentMap<Long, Optional<Double>>> sma){
+		throw new Exception("Not implemented.");		
+	} */
+	
+	//TODO
+	@Deprecated//??????? not sure if needed
+	public static 		ConcurrentLinkedDeque<Long> getPeriodsToCheck(final ConcurrentMap<Long, ConcurrentMap<Long, Optional<Double>>> sma){
+		ConcurrentLinkedDeque<Long> periodsToCheck = new ConcurrentLinkedDeque<>();
+		final long maxPeriod = sma.keySet().stream().count();
+		long periodToAdd = 0;
+		while(periodToAdd < maxPeriod){
+			periodsToCheck.addLast(periodToAdd);
+			periodToAdd++;
+		}
+		return periodsToCheck;
 	}
 	
 	@SuppressWarnings("unused")
